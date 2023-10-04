3df6124ae0f4a334cd47596f867201bae36dd942
KamyaPA <peteraksel@live.dk>
2023-10-04 15:09:51 +0200

    Added fs, dialog to the alowlist. Open browser console on debug builds

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+pub mod project;
+use project::*;
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+        #[cfg(debug_assertions)]
+        {
+            app.get_window("main").unwrap().open_devtools();
+        }
+        Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![
+        load_project,
+        save_project
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
